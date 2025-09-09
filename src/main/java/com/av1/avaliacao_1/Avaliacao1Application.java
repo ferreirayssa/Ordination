51dd752ac2910e244c514a718f7693a8efa17921
xneesany <xneesan07@gmail.com>
2025-09-09 19:31:01 -0300

    Primeiro commit

---
+++ src/main/java/com/av1/avaliacao_1/Avaliacao1Application.java
@@ -0,0 +1,13 @@
+package com.av1.avaliacao_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Avaliacao1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Avaliacao1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/av1/avaliacao_1/Avaliacao1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.av1.avaliacao_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Avaliacao1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
